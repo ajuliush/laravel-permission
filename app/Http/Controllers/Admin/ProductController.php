15097f9bb885e85e7e35076180fb7a0c2052a84f
ajuliush <ajuliush@gmail.com>
2024-01-02 18:05:22 +0600

    add: product show

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -95,7 +95,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return view('admin.product.show',get_defined_vars());
     }
 
     /**
